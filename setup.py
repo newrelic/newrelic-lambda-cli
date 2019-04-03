eba4b0f6d158ee7ef2f2885928b2eacc92c5caa9
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2019-04-02 20:16:32 -0400

    rename to iopipe-cli and entrypoint `iopipe` cmd
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='iopipe_install',
+    name='iopipe-cli',
     version='0.1',
     python_requires='>=3.3',
     packages=find_packages(),
@@ -13,6 +13,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        iopipe-install=iopipe_install.cli:main
+        iopipe=iopipe_cli.cli:main
     ''',
 )
