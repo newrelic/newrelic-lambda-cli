6e035ccd76cf39f308ee6d516f7588e75d2df55d
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2019-08-13 11:14:49 -0400

    Release 0.1.6 with revert of ci change from April
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (ImportError, OSError):
 
 setup(
     name='iopipe-cli',
-    version='0.1.5',
+    version='0.1.6',
     python_requires='>=3.3',
     description="cli utility for managing and instrumenting serverless applications",
     long_description=README,
