801c559c602b0409682892b7e81210b0f267acb7
Michael Lavers <kolanos@gmail.com>
2019-10-24 11:55:24 -0700

    Initial commit

--- iopipe_cli/layers.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def index(region, runtime):
-    req = requests.get(
-        f"https://{region}.layers.iopipe.com/get-layers?CompatibleRuntime={runtime}"
-    )
-    layers_response = req.json()
-    return layers_response.get("Layers", [])
