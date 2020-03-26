d9b724a49703a6f0dd6dc33a4094aad3af3e81d6
Michael Lavers <kolanos@gmail.com>
2020-03-26 09:25:57 -0700

    Remove RST README Conversion (#89)

--- setup.py
+++ setup.py
@@ -2,20 +2,15 @@ import os
 
 from setuptools import find_packages, setup
 
-try:
-    from pypandoc import convert
-
-    README = convert("README.md", "rst")
-except (ImportError, OSError):
-    README = open(os.path.join(os.path.dirname(__file__), "README.md"), "r").read()
+README = open(os.path.join(os.path.dirname(__file__), "README.md"), "r").read()
 
 setup(
     name="newrelic-lambda-cli",
-    version="0.1.25",
+    version="0.1.26",
     python_requires=">=3.3",
     description="A CLI to install the New Relic AWS Lambda integration and layers.",
     long_description=README,
-    long_description_content_type="text/x-rst",
+    long_description_content_type="text/markdown",
     author="New Relic",
     author_email="serverless-dev@newrelic.com",
     url="https://github.com/newrelic/newrelic-lambda-cli",
