68d876bb97ef829bde814ac5584e204d60c7e022
sashwatdas <sashwatdas@newrelic.com>
2025-05-27 16:22:08 +0530

    feat: added changes

--- newrelic_lambda_cli/layers.py
+++ newrelic_lambda_cli/layers.py
@@ -314,18 +314,21 @@ def install(input, function_arn):
             click.echo(json.dumps(res, indent=2))
 
         old_layers = config["Configuration"].get("Layers", [])
-        old_layer_arn = old_layers[0]["Arn"] if old_layers else "None"
+        old_layer_arn = old_layers[0]["Arn"].rsplit(":", 1)[0] if old_layers else "None"
+        old_layer_version = old_layers[0]["Arn"].split(":")[-1] if old_layers else "None"
+        new_layer = update_kwargs["Layers"][0]
+        new_layer_arn = update_kwargs["Layers"][0].rsplit(":", 1)[0]
+        new_layer_version = update_kwargs["Layers"][0].split(":")[-1]
 
-        new_layer_arn = update_kwargs["Layers"][0]
         if old_layer_arn == "None":
             success(
-            "Successfully installed Layer ARN %s to the function: %s"
-            % (new_layer_arn, function_arn)
-        )
-        else : 
+                "Successfully installed Layer ARN %s for the function: %s"
+                % (new_layer, function_arn)
+            )
+        else:
             success(
-                "Successfully installed new layer\nfrom %s to %s to the function: %s"
-                % (old_layer_arn, new_layer_arn, function_arn)
+                "Successfully upgraded Layer ARN %s from version: %s to version: %s for the function: %s"
+                % (new_layer_arn, old_layer_version, new_layer_version, function_arn)
             )
         return True
 
@@ -427,7 +430,7 @@ def uninstall(input, function_arn):
 
         old_layers = config["Configuration"].get("Layers", [])
         old_layer_arn = old_layers[0]["Arn"] if old_layers else "None"
-        success("Successfully uninstalled layer %s from %s" % (old_layer_arn,function_arn))
+        success("Successfully uninstalled Layer %s from %s" % (old_layer_arn,function_arn))
         return True
 
 
